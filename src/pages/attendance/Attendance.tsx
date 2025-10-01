805e8eb4be841553e652951d4eb39a9b03fb5cb8
Kamoliddin Mirzaboyev <kamoliddinmirzaboyev2005@gmail.com>
2025-10-01 21:40:34 +0500

    Add some pages: Dormitory, studentprofile

--- src/pages/attendance/Attendance.tsx
+++ src/pages/attendance/Attendance.tsx
@@ -6,7 +6,7 @@ function Attendance() {
       <div className="w-full ">
         <div className="pageTitle flex items-center justify-between mb-6">
           <h2 className="text-3xl font-semibold">Davomat nazorat</h2>
-          <button className="bg-blue-600 flex items-center py-2 px-2 items-center gap-2 text-white rounded">
+          <button className="bg-blue-600 flex  py-2 px-2 items-center gap-2 text-white rounded">
             <MdOutlinePersonAdd className="text-xl" />
             Qavat sardori qo'shish
           </button>
